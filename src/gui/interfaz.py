8a42e70e4a8ff4a8c0492cec899db4a41e6d78a8
viiiccv <95257527+viiiccv@users.noreply.github.com>
2023-12-07 23:49:42 +0100

    botones implementados y el camino se dibuja poco a poco

--- src/gui/interfaz.py
+++ src/gui/interfaz.py
@@ -1,34 +1,22 @@
-#Necesario métodos:
-
-#1: Imprimir grafo "a secas"
-
-#2: Imprimir grafo resaltando aristas de la ruta
-
-#3: Posible método borrar, puede que solape con 1
-
-
 import matplotlib.pyplot as plt     #Librería gráfica
 import networkx as nx   #Librería de grafos
 
-# import tkinter as tk
 import backend.ruta as ruta
 from backend import cargar_grafo as cg
 import pygame
 import os
+import time
 
 FICH_NODOS = "gui/coord.txt"
-# FICH_NODOS = "backend/nodos.txt"
 
 def dibujar(g):
     
     pygame.init()
-    size = (750,724)
+    size = (800,724)
     screen = pygame.display.set_mode(size)
 
     img_path = "gui/plano.jpg" # plano del metro sobre el que dibujamos
     image = pygame.image.load(img_path).convert()
-    # coordenadas = g.dict_nodos.get("Cuire") # estas coord no me sirven
-    # print(coordenadas)
 
     coord_nodos = [
          (85, 586), (184, 539), (224, 500), (263, 460), (302, 423), (330, 358), (329, 276), (400, 255), (418, 211)
@@ -37,12 +25,15 @@ def dibujar(g):
         (143,370),(186, 331),(217, 298),(230, 232),(226, 178),(295, 161),
         (366, 161),(437, 163),(497, 159),(555, 159),(594, 195),(627, 233),(668, 271),(728, 308)]
 
-    # Create a font object
-    font = pygame.font.Font(None, 36) # cambiar el font del texto y poner botones
+    coord_botones = [(705, 575),(729, 575)]
+    
+    # creo los txts de mensajes que van saliendo por pantalla
+    font = pygame.font.Font(None, 36)
     textoPartida = font.render("Elija una estación de partida.",True, (0,0,0), (255,255,255))
     textoDestino = font.render("Elija una estación de destino.",True, (0,0,0), (255,255,255))
-    textoRuta = font.render("Ruta mas corta.",True, (0,0,0), (255,255,255))
-    
+    textoModo = font.render("Elija el modo que desee:         ",True, (0,0,0), (255,255,255))
+    textoFin = font.render("¡Buen viaje!",True, (0,0,0), (255,255,255))
+
     # Clear the screen
     screen.fill((255, 255, 255))
 
@@ -52,11 +43,29 @@ def dibujar(g):
     # Main game loop
     running = True
     pantallaInicio = True
+
+    botonDist = Boton(654, 439, 100, 40, (123, 175, 212), (255, 0, 0), "Distancia")
+    botonTras = Boton(654, 513, 100, 40, (123, 175, 212), (255, 0, 0), "Trasbordo")
+    botonRein = Boton(654, 587, 100, 40, (123, 175, 212), (255, 0, 0), "Reiniciar")
+
     while running:
-        if pantallaInicio:
+        if pantallaInicio: # es la pantalla pp, en la que escoges estacionInicio
+            
+            # si reinicio la pantalla, vuelvo a dibujar el plano y empiezo de nuevo (sino se pinta encima del otro)
+            screen.fill((255, 255, 255))
+            screen.blit(image, (0, 0))
+
             for pos in coord_nodos:
                 pygame.draw.circle(screen, (255, 255, 255), pos, 6)  # White circle
                 pygame.draw.circle(screen, (0, 0, 0), pos, 8, 2)  # Black border
+
+            # restablecer el color de los botones, cuando pulsas uno se vuelve rojo
+            botonDist.color_normal = (123, 175, 212)
+            botonTras.color_normal = (123, 175, 212)
+            botonRein.color_normal = (123, 175, 212)
+            botonDist.dibujar(screen)
+            botonTras.dibujar(screen)
+            botonRein.dibujar(screen)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -65,31 +74,48 @@ def dibujar(g):
                     pos = pygame.mouse.get_pos()
                     for circle_pos in coord_nodos:
                         if pygame.Rect(circle_pos[0] - 10, circle_pos[1] - 10, 20, 20).collidepoint(pos):
-                            # print("Estacion de partida: ", circle_pos)
                             nodoInicio = nombreNodo(circle_pos)
                             pygame.draw.circle(screen, (255, 0, 0), circle_pos, 9)  # White circle
                             pygame.draw.circle(screen, (0, 0, 0), circle_pos, 9, 2)  # Black border
                             estacionPartida = True
                             pantallaInicio = False
 
-        elif estacionPartida:
+        elif estacionPartida: # ya se ha elegido la estacPartida, ahora escoger la de destino
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.MOUSEBUTTONUP:
                     pos = pygame.mouse.get_pos()
-                    # Check if the click is inside any of the circles
                     for circle_pos in coord_nodos:
                         if pygame.Rect(circle_pos[0] - 10, circle_pos[1] - 10, 20, 20).collidepoint(pos):
-                            nodoDestino = nombreNodo(circle_pos)
-                            pygame.draw.circle(screen, (255, 0, 0), circle_pos, 9)  # White circle
-                            pygame.draw.circle(screen, (0, 0, 0), circle_pos, 9, 2)  # Black border
-                            estacionPartida = False
-                            estacionLlegada = True
-                        
-        elif estacionLlegada:
-            l = ruta.ruta(g, nodoInicio, nodoDestino, "distancia")
-            
+                            if (nombreNodo(circle_pos)!= nodoInicio):
+                                nodoDestino = nombreNodo(circle_pos)
+                                pygame.draw.circle(screen, (255, 0, 0), circle_pos, 9)  # White circle
+                                pygame.draw.circle(screen, (0, 0, 0), circle_pos, 9, 2)  # Black border
+                                estacionPartida = False
+                                botonRuta = True
+        elif botonRuta: # una vez elegidas ambas estaciones, escoges el modo (distancia o trasbordos)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    if botonDist.pulsado():
+                        botonDist.color_normal = botonDist.color_pulsado
+                        botonDist.dibujar(screen)
+                        l = ruta.ruta(g, nodoInicio, nodoDestino, "distancia")
+                        botonRuta = False
+                        dibujaRuta= True
+                    if botonTras.pulsado():
+                        botonTras.color_normal = botonTras.color_pulsado
+                        botonTras.dibujar(screen)
+                        l = ruta.ruta(g, nodoInicio, nodoDestino, "trasbordo")
+                        botonRuta = False
+                        dibujaRuta= True
+                    if botonRein.pulsado():
+                        botonRuta = False
+                        pantallaInicio = True
+
+        elif dibujaRuta: # dibuja la ruta en el mapa
             posiciones_deseadas = {}
             for nodo in l:
             # Leo el archivo y pongo las coordenadas
@@ -105,26 +131,33 @@ def dibujar(g):
                                     x = int(palabras[1])
                                     y = int(palabras[2])
                                     circle_pos = (x, y)
-                                    # print(circle_pos)
-                                    pygame.draw.circle(screen, (255, 0, 0), circle_pos, 9)  # White circle
+                                    pygame.draw.circle(screen, (255, 0, 0), circle_pos, 9)  # Red circle
                                     pygame.draw.circle(screen, (0, 0, 0), circle_pos, 9, 2)  # Black border
+                                    pygame.display.flip() 
+                                    time.sleep(0.1)
+                                    
             
-            
-            estacionLlegada = False
+            dibujaRuta= False
             pantallaFinal =True
     
-        elif pantallaFinal:
+        elif pantallaFinal: # puedes pulsar reiniciar y volver a empezar
              for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.MOUSEBUTTONUP:
-                    pantallaFinal = False
-                    pantallaInicio = True
+                    if botonRein.pulsado():
+                        pantallaFinal = False
+                        pantallaInicio = True
 
         if pantallaInicio:
             screen.blit(textoPartida,(370,60))
         elif estacionPartida:
             screen.blit(textoDestino,(370,60))
+        elif botonRuta:
+            screen.blit(textoModo,(370,60))
+        elif dibujaRuta:
+            pygame.draw.rect(screen, (255, 255, 255), (370, 60, 800, 50))
+            screen.blit(textoFin,(500,60))
 
         # Update the display
         pygame.display.flip()
@@ -146,3 +179,22 @@ def nombreNodo (coord):
                         if x == coord[0] and y == coord[1]:
                             nombre = palabras[0]
                             return nombre
+                        
+class Boton:
+    def __init__(self, x, y, ancho, alto, color_normal, color_pulsado, texto, font_size=25):
+        self.rect = pygame.Rect(x, y, ancho, alto)
+        self.color_normal = color_normal
+        self.color_pulsado = color_pulsado
+        self.texto = texto
+        self.font = pygame.font.Font(None, font_size)
+
+    def dibujar(self, screen):
+        pygame.draw.rect(screen, self.color_normal, self.rect)
+        pygame.draw.rect(screen, (255,255,255), self.rect, 1) 
+
+        text_surface = self.font.render(self.texto, True, (255,255,255))
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        screen.blit(text_surface, text_rect)
+
+    def pulsado(self):
+        return self.rect.collidepoint(pygame.mouse.get_pos())
